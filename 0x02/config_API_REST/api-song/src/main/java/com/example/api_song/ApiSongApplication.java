7bbb296047a74542b24b50626c979974e9dae3b3
Walter Pedro <wal100lp@hotmail.com>
2025-10-10 11:43:04 +0000

    Implementing 'Java Microsserviços - Construindo uma API REST c/ Spring Boot' Exercise 0

---
+++ 0x02/config_API_REST/api-song/src/main/java/com/example/api_song/ApiSongApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSongApplication.class, args);
+	}
+
+}
