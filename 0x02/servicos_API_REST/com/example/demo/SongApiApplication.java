059d65e8f6da77797391cf9f247aeacf21647e69
Walter Pedro <wal100lp@hotmail.com>
2025-10-10 14:22:35 +0000

    Fix Spring class name

--- 0x02/servicos_API_REST/com/example/demo/SongApiApplication.java
+++ 0x02/servicos_API_REST/com/example/demo/SongApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiSongApplication {
+public class SongApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSongApplication.class, args);
