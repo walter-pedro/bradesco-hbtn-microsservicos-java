52cd91c38657ea017ee320ca614f9969e36d1b52
Walter Pedro <54921496+walter-pedro@users.noreply.github.com>
2025-10-30 17:31:27 -0300

    Fix classes names and annotations

--- 0x07/api-rest-calculator/src/main/java/com/example/calculator/CalculatorApiApplication.java
+++ 0x07/api-rest-calculator/src/main/java/com/example/calculator/CalculatorApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRestCalculatorApplication {
+public class CalculatorApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiRestCalculatorApplication.class, args);
+		SpringApplication.run(CalculatorApiApplication.class, args);
 	}
 
 }
